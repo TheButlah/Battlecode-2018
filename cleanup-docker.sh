ade6688304caf4096e1e6028c4930ec42f160ca1
Chesley Tan <chesleytan97@gmail.com>
2018-01-10 00:44:51 -0500

    Add docker cleanup script

---
+++ cleanup-docker.sh
@@ -0,0 +1,4 @@
+# Delete all non-running containers
+sudo docker ps -aq --no-trunc | sudo xargs docker rm
+# Delete all volumes not associated with a container
+sudo docker volume rm $(sudo docker volume ls -qf dangling=true)
