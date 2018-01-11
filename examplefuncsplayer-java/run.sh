14d610542eef351a1e5ecd2b9c0c0e750b6993d8
Ryan Butler <thebutlah@gmail.com>
2018-01-10 22:46:07 -0600

    Fixed name of jar in run.sh

--- examplefuncsplayer-java/run.sh
+++ examplefuncsplayer-java/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # build the java files.
 # there will eventually be a separate build step, but for now the build counts against your time.
-java -classpath /battlecode-java:. -jar Battlecode.jar
+java -classpath /battlecode-java:. -jar Battlecode-2018-0.1-all.jar
