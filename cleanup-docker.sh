3465ee5a60848eda74d1598f7396633c7f703359
Chesley Tan <chesleytan97@gmail.com>
2018-01-11 17:09:42 -0500

    Update docker cleanup script

--- cleanup-docker.sh
+++ cleanup-docker.sh
@@ -2,3 +2,5 @@
 sudo docker ps -aq --no-trunc | sudo xargs docker rm
 # Delete all volumes not associated with a container
 sudo docker volume rm $(sudo docker volume ls -qf dangling=true)
+# Delete all unused images
+sudo docker image prune
