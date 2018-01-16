e947701c5b936d66ac69323ef1118a23b3b6ad1c
Chesley Tan <chesleytan97@gmail.com>
2018-01-16 12:02:50 -0500

    Fix knight target disengage; Make spawn target selection dynamic

--- src/main/java/org/battlecode/bc18/bots/noobbot/Knight.java
+++ src/main/java/org/battlecode/bc18/bots/noobbot/Knight.java
@@ -8,15 +8,13 @@ import org.battlecode.bc18.util.Utils;
 
 import bc.Direction;
 import bc.MapLocation;
-import bc.Planet;
-import bc.PlanetMap;
 import bc.Unit;
 import bc.UnitType;
-import bc.VecUnit;
 
 public class Knight extends AKnight {
 
     private Unit target = null; //Although this doesn't update, it will allow us to go to last seen spot.
+    private int spawnTargetSeed;
 
     /**
      * Constructor for Knight.
@@ -25,6 +23,7 @@ public class Knight extends AKnight {
     Knight(Unit unit) {
         super(unit);
         assert unit.unitType() == UnitType.Knight;
+        spawnTargetSeed = Utils.rand.nextInt(Integer.MAX_VALUE);
     }
 
     @Override
@@ -41,6 +40,9 @@ public class Knight extends AKnight {
 
         MapLocation myMapLoc = getMapLocation();
 
+        if (hasTarget() && !Utils.gc.canSenseUnit(target.id())) {
+            target = null;
+        }
         // Get closest enemy MyUnit
         Unit closestUnit = null;
         int closestUnitDist = Integer.MAX_VALUE;
@@ -54,14 +56,17 @@ public class Knight extends AKnight {
             }
         }
         if (closestUnit != null) {
-            this.target = closestUnit;
+            target = closestUnit;
         }
 
         if (isMoveReady()) {
+            println(Main.enemySpawns.size());
+            println("hasTarget: " + hasTarget());
             boolean moved = false;
             // If we have a target, move towards it
             if (hasTarget()) {
-                MapLocation targetEnemy = this.target.location().mapLocation();
+                println("target loc: " + target.location().mapLocation());
+                MapLocation targetEnemy = target.location().mapLocation();
                 int[][] distances = PathFinding.earthPathfinder.search(targetEnemy.getY(), targetEnemy.getX());
                 Direction towardsEnemy = PathFinding.moveDirectionToDestination(distances, myMapLoc);
                 //Already did `isMoveReady()` so instead of doing `canMove()` we just do `isAccessible()`
@@ -69,27 +74,26 @@ public class Knight extends AKnight {
                     move(towardsEnemy);
                     moved = true;
                 }
-            } else { // Otherwise, we move towards the enemy spawn
+            }
+            else if (Main.enemySpawns.size() != 0) { // Otherwise, we move towards the enemy spawn
                 // TODO: this assumes the unit is on earth
-                if (Utils.gc.round() > 50) {
-                    PlanetMap initialMap = Utils.gc.startingMap(Planet.Earth);
-                    VecUnit initialUnits = initialMap.getInitial_units();
-                    MapLocation spawnLoc = null;
-                    for (int i = 0; i < initialUnits.size(); ++i) {
-                        Unit unit = initialUnits.get(i);
-                        if (unit.team() != getTeam()) {
-                            spawnLoc = unit.location().mapLocation();
-                            break;
-                        }
+                MapLocation spawnTarget = Main.enemySpawns.get(spawnTargetSeed % Main.enemySpawns.size());
+                if (spawnTarget.equals(myMapLoc)) {
+                    // We are already at the enemy spawn and we see no enemies, so we will
+                    // conclude that the enemy spawn has been eliminated
+                    Main.enemySpawns.remove(spawnTarget);
+                    // Get next spawn target
+                    if (Main.enemySpawns.size() != 0) {
+                        Main.enemySpawns.get(spawnTargetSeed % Main.enemySpawns.size());
                     }
-                    if (spawnLoc != null) {
-                        int[][] distances = PathFinding.earthPathfinder.search(spawnLoc.getY(), spawnLoc.getX());
-                        Direction towardsEnemy = PathFinding.moveDirectionToDestination(distances, myMapLoc);
-                        //Already did `isMoveReady()` so instead of doing `canMove()` we just do `isAccessible()`
-                        if (towardsEnemy != Direction.Center && isAccessible(towardsEnemy)) {
-                            move(towardsEnemy);
-                            moved = true;
-                        }
+                }
+                if (Utils.gc.round() > 50 && spawnTarget != null) {
+                    int[][] distances = PathFinding.earthPathfinder.search(spawnTarget.getY(), spawnTarget.getX());
+                    Direction towardsEnemy = PathFinding.moveDirectionToDestination(distances, myMapLoc);
+                    //Already did `isMoveReady()` so instead of doing `canMove()` we just do `isAccessible()`
+                    if (towardsEnemy != Direction.Center && isAccessible(towardsEnemy)) {
+                        move(towardsEnemy);
+                        moved = true;
                     }
                 }
             }
@@ -102,7 +106,7 @@ public class Knight extends AKnight {
                     if (isAccessible(dir)) {
                         //println("Moving");
                         move(dir);
-                        return;
+                        break;
                     }
                 }
             }
@@ -124,6 +128,6 @@ public class Knight extends AKnight {
     }
 
     private boolean hasTarget() {
-        return this.target != null;
+        return target != null;
     }
 }
--- src/main/java/org/battlecode/bc18/bots/noobbot/Main.java
+++ src/main/java/org/battlecode/bc18/bots/noobbot/Main.java
@@ -2,19 +2,25 @@ package org.battlecode.bc18.bots.noobbot;
 
 import static org.battlecode.bc18.util.Utils.gc;
 
+import java.util.ArrayList;
+
 import org.battlecode.bc18.PathFinding;
 import org.battlecode.bc18.api.AUnit;
 import org.battlecode.bc18.util.Utils;
 
 import bc.GameController;
+import bc.MapLocation;
 import bc.Planet;
 import bc.PlanetMap;
+import bc.Team;
+import bc.Unit;
 import bc.UnitType;
+import bc.VecUnit;
 
 public class Main {
+    public static ArrayList<MapLocation> enemySpawns;
 
     public static void main(String[] args) {
-
         //Without this, MyUnit cannot form new units
         AUnit.init(new UnitBuilder());
 
@@ -30,9 +36,19 @@ public class Main {
         gc.queueResearch(UnitType.Worker);
 
         if (gc.planet() == Planet.Earth) {
-            PlanetMap earthMap = gc.startingMap(Planet.Earth);
-            Utils.setEarthDims(earthMap);
-            PathFinding.initializeEarthPathfinder(earthMap);
+            // Get enemy spawns
+            enemySpawns = new ArrayList<>();
+            PlanetMap initialMap = Utils.EARTH_START;
+            VecUnit initialUnits = initialMap.getInitial_units();
+            for (int i = 0; i < initialUnits.size(); ++i) {
+                Unit unit = initialUnits.get(i);
+                if (unit.team() != Utils.TEAM) {
+                    enemySpawns.add(unit.location().mapLocation());
+                }
+            }
+
+            // Set pathfinding weights
+            PathFinding.initializeEarthPathfinder(initialMap);
         }
 
         while (true) {
