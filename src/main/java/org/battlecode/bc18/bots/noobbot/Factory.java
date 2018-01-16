015853b5d0dd7fcb6b7ca88733b5325ea870a462
Chesley Tan <chesleytan97@gmail.com>
2018-01-16 13:37:18 -0500

    Optimize factory unloading

--- src/main/java/org/battlecode/bc18/bots/noobbot/Factory.java
+++ src/main/java/org/battlecode/bc18/bots/noobbot/Factory.java
@@ -8,6 +8,8 @@ import org.battlecode.bc18.util.Utils;
 import java.util.List;
 
 public class Factory extends AFactory {
+    //static int time1, time2, time3;
+    //static long startTime;
 
     /**
      * Constructor for Factory.
@@ -23,19 +25,32 @@ public class Factory extends AFactory {
         // Since we cant maintain the invariant for the units HashMap, manually add in units to ensure invariant.
 
         // make and place knights until you can't :D
+        //startTime = System.currentTimeMillis();
         List<MyUnit> nearbyWorkers = senseNearbyFriendlies(UnitType.Worker);
         boolean hasNearbyWorker = (nearbyWorkers.size() >=1);
+        //time1 += System.currentTimeMillis() - startTime;
+        //System.out.println("time1: " + time1);
 
+        //startTime = System.currentTimeMillis();
         if (!hasNearbyWorker && canProduceRobot(UnitType.Worker)) {
             produceRobot(UnitType.Worker);
         } else if (canProduceRobot(UnitType.Knight)) {
             produceRobot(UnitType.Knight);
         }
+        //time2 += System.currentTimeMillis() - startTime;
+        //System.out.println("time2: " + time2);
+        //startTime = System.currentTimeMillis();
         // Unload units
-        for (Direction dir : Utils.dirs) {
-            if (canUnload(dir)) unload(dir);
-            //No break here because I think you can unload multiple units?
+        if (getGarrison().size() != 0) {
+            for (Direction dir : Utils.dirs) {
+                if (canUnload(dir)) {
+                    unload(dir);
+                    break;
+                }
+            }
         }
+        //time3 += System.currentTimeMillis() - startTime;
+        //System.out.println("time3: " + time3);
     }
 
     @Override
